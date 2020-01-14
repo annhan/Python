84b7d5658c51a449105fb729fffd6cb0063ac0b5
Riki <annhandt09@gmail.com>
2020-01-14 09:27:31 +0700

    up

--- Advantech/Cai mySQL.md
+++ Advantech/Cai mySQL.md
@@ -74,3 +74,76 @@ threading  và process
 ------------
 
 Khác biệt process ứng dụng cho nhiều core, bộ nhớ không liên thông nên không dùng chung bộ nhớ với process khác. Phaoir đung queue của multiprocess
+
+
+-----------
+AutoStart
+--------------------
+Ba phương pháp được đề cập trong hướng dẫn này là:
+•	RC.local - Có thể là cách dễ nhất và đơn giản nhất để chương trình của bạn chạy khi khởi động. Nhược điểm là các tác vụ bắt đầu với RC.local xảy ra trước khi hệ thống windows X khởi động, điều đó có nghĩa là bạn sẽ không có quyền truy cập vào các yếu tố giao diện người dùng đồ họa (GUI).
+•	autostart - Được sử dụng để tự động chạy các chương trình của bạn sau khi LXDE (môi trường máy tính để bàn được sử dụng bởi Raspbian) bắt đầu. Nó hơi phức tạp hơn một chút so với RC.local, nhưng nó cho phép bạn chạy các chương trình yêu cầu các yếu tố đồ họa.
+•	systemd - Cách mới và phổ biến để tự động khởi động các chương trình trong Linux. Đây chắc chắn là phức tạp nhất trong ba, nhưng nó cho phép bạn chạy trước khi LXDE bắt đầu, đợi cho đến khi bạn có quyền truy cập vào các quy trình khác (ví dụ: mạng, máy tính để bàn đồ họa) hoặc đơn giản là khởi động lại chương trình của bạn nhiều lần cho đến khi nó hoạt động. Như vậy, đây là một cách mạnh mẽ để tạo và quản lý các dịch vụ chạy trong nền.
+
+- Cách systemd
+Với Gui:
+Tạo một tệp .service mới trong thư mục systemd:
+SAO CHÉP MÃ
+```
+sudo nano /lib/systemd/system/clock.service
+```
+Nhập văn bản sau vào tài liệu:
+SAO CHÉP MÃ
+```
+[Unit]
+Description=Start Clock
+
+[Service]
+Environment=DISPLAY=:0
+Environment=XAUTHORITY=/home/pi/.Xauthority
+ExecStart=/usr/bin/python3 /home/pi/clock.py
+Restart=always
+RestartSec=10s
+KillMode=process
+TimeoutSec=infinity
+
+[Install]
+WantedBy=graphical.target
+```
+
+Không cần Gui thi 
+Tạo một tệp .service mới trong thư mục systemd:
+SAO CHÉP MÃ
+```
+sudo nano /lib/systemd/system/blink.service
+```
+Nhập văn bản sau vào tài liệu:
+SAO CHÉP MÃ
+```
+[Unit]
+Description=Blink my LED
+After=multi-user.target
+
+[Service]
+ExecStart=/usr/bin/python3 /home/pi/blink.py
+
+[Install]
+WantedBy=multi-user.target
+```
+
+
+Các lệnh.
+```
+sudo systemctl daemon-reload
+
+sudo systemctl enable clock.service
+
+systemctl status clock.service
+
+sudo systemctl stop clock.service
+sudo systemctl start clock.service
+```
+
+
+
+Điều này có thể hữu ích để khởi động lại một dịch vụ nếu bạn đã thay đổi dịch vụ mà không phải khởi động lại hệ thống. Chỉ cần nhớ chạy sudo systemctl daemon-reloadnếu bạn thực hiện bất kỳ thay đổi nào đối với tệp .service!
+
--- Advantech/mqttfile.py
+++ Advantech/mqttfile.py
@@ -3,7 +3,10 @@ import threading
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
 import json
+import datetime
 class MyMQTTClass(mqtt.Client):
+    statusold = False
+    statusold1 = False
     def on_connect(self, mqttc, obj, flags, rc):
         if (rc == 0):
             print("connection successful broker linked")
@@ -25,30 +28,36 @@ class MyMQTTClass(mqtt.Client):
             print("loi subrice")
 
     def on_message(self, mqttc, obj, msg):
-        print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+        #print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
         global i
         bien = "00D0C9E27959"
-        print(msg.topic + " " + str(msg.payload))
-        y = json.loads(msg.payload)
-        print(y["di1"])
-        if (y["di1"] == False):
-            print "Fals roi"
-            infot = self.publish("Advantech/%s/ctl/do1" % (bien), '{"v":true}')
-            infot.wait_for_publish()
-        else:
-            print "OK ROI"
-            try:
-                infot = self.publish("Advantech/%s/ctl/do1" % (bien), '{"v":false}')
-            except:
-                print("loi send")
+        try:
+            y = json.loads(msg.payload)
+            if (y["di1"] == False):
+                if (self.statusold== False):
+                    self.statusold1 = False
+                    self.statusold = True
+                    print("%s - False"%(datetime.datetime.now()))
+                    infot = self.publish("Advantech/%s/ctl/do1" % (bien), '{"v":false}', qos=1)
+            else:
+                if (self.statusold1== False):
+                    self.statusold = False
+                    self.statusold1 = True
+                    print("%s - OK" % (datetime.datetime.now()))
+                    infot = self.publish("Advantech/%s/ctl/do1" % (bien), '{"v":true}', qos=1)
+        except:
+            pass
     def on_publish(self, mqttc, obj, mid):
-        print("mid: "+str(mid))
+        #print("mid: "+str(mid))
+        pass
 
     def on_subscribe(self, mqttc, obj, mid, granted_qos):
-        print("Subscribed: "+str(mid)+" "+str(granted_qos))
+        #print("Subscribed: "+str(mid)+" "+str(granted_qos))
+        pass
 
     def on_log(self, mqttc, obj, level, string):
-        print(string)
+        #print(string)
+        pass
 
     def run(self):
         self.username_pw_set("mhome", password="Chotronniemvui1")
@@ -59,7 +68,7 @@ class MyMQTTClass(mqtt.Client):
                 self.loop_forever()
             except:
                 print "d"
-# Advantech/MAC_of_WISE/Device_Status
+#           Advantech/MAC_of_WISE/Device_Status
 # subscribe Advantech/00D0C9E27D59E/data {"s":8,"t":"2020-01-13T06:56:09Z","q":192,"c":0,"di1":false,"di2":false,"di3":false,"di4":false,"di5":false,"di6":false,"di7":false,"di8":false}
 # publish   Advantech/00D0C9E27959/ctl/do1 {"v":true} / {"v":false}
 class Test(threading.Thread):
